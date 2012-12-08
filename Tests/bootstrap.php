83ec16249e0ff401f12a742a1efe127c7b274ad3
Marc Bontje <marc@rednose.nl>
2012-12-08 16:59:14 +0100

    Test

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -12,4 +12,5 @@
 if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
     throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
 }
+
 require $autoloadFile;
