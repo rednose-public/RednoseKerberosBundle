23fe1a4d727898739bc33c04afcc1b9a0aabb8a8
Marc Bontje <marc@rednose.nl>
2014-05-07 20:33:05 +0200

    Updated license headers

--- DependencyInjection/RednoseKerberosExtension.php
+++ DependencyInjection/RednoseKerberosExtension.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the RednosetKerberosBundle package.
+ * This file is part of the RednoseKerberosBundle package.
  *
- * (c) RedNose <info@rednose.nl>
+ * (c) RedNose <http://www.rednose.nl>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- Tests/Security/Authentication/Provider/KerberosProviderTest.php
+++ Tests/Security/Authentication/Provider/KerberosProviderTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the RednoseKerberosBundle package.
+ *
+ * (c) RedNose <http://www.rednose.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Rednose\KerberosBundle\Tests\Security\Authentication\Provider;
 
 use Rednose\KerberosBundle\Security\Authentication\Provider\KerberosProvider;
